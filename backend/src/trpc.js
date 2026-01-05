9b2f70f0634829f5ccd06630573c4b389b99bdeb
QueZera <zaherzulkifly@gmail.com>
2026-01-05 10:11:47 +0800

    trpc dh ada dlm utils, and create ni kt front end je

--- backend/src/trpc.js
+++
@@ -1,7 +0,0 @@
-import { createTRPCReact } from '@trpc/react-query';
-
-/**
- * We only import the TYPE of your router. 
- * This ensures no backend code is accidentally bundled with your frontend.
- */
-export const trpc = createTRPCReact();
--- src/trpc.js
+++
@@ -1,4 +0,0 @@
-import { createTRPCReact } from '@trpc/react-query';
-
-// This initializes the tRPC hooks for your frontend
-export const trpc = createTRPCReact();
