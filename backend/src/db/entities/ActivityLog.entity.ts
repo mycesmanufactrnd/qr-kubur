f7037b1af7767ccedbf235c2e79da463b88a8a8d
QueZera <zaherzulkifly@gmail.com>
2026-01-19 14:44:55 +0800

    entity update for tahlil and donation

--- backend/src/db/entities/ActivityLog.entity.ts
+++ backend/src/db/entities/ActivityLog.entity.ts
@@ -7,18 +7,26 @@ export class ActivityLog {
 
     @Column("varchar", { length: 255, nullable: true })
     activitytype?: string;
+    // define which activity
     
     @Column("varchar", { length: 255, nullable: true })
     functionname?: string;
+    // define which function it call from
     
     @Column("varchar", { length: 255, nullable: true })
     useremail?: string;
     
     @Column("varchar", { length: 255, nullable: true })
     level?: string;
+    // info, warn, error, debug
     
-    @Column("varchar", { length: 255, nullable: true })
+    @Column("text", { nullable: true })
     summary?: string;
+    // messages, if error, use error message at catch
+    
+    @Column("text", { nullable: true })
+    extramessage?: string;
+    // extra messages, if error, use error message at catch
 
     @CreateDateColumn({ name: "createdat" })
     createdat!: Date;
